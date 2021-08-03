73879eebc7cecb1c4959d8d77c8fca18c9f5b1a2
aoaae <s.suksawat@webmail.npru.ac.th>
2021-08-03 19:09:48 +0700

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Welcome to Java Programming");
+        System.out.println("Today is Tueday");
+        System.out.println("English for Computer");
     }
 }
